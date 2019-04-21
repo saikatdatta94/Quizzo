f573543e3a6989f74c527ef5eda610a01f9588bc
Saikat Datta <saikatdatta94@gmail.com>
2019-04-22 01:18:06 +0530

    Subcategory landing page layout is modified.Now going to implement recyclerView from firestore

--- app/src/main/java/com/example/saikat/quizzo/QuestionActivity.java
+++ app/src/main/java/com/example/saikat/quizzo/QuestionActivity.java
@@ -1,25 +1,71 @@
 package com.example.saikat.quizzo;
 
+import android.content.res.ColorStateList;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class QuestionActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
 
+
+        FloatingActionButton followButton = findViewById(R.id.follow_button);
+        final Toolbar toolbar = findViewById(R.id.question_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        CollapsingToolbarLayout collapsingToolbarLayout = findViewById(R.id.toolbar_layout);
+        collapsingToolbarLayout.setTitle("My title");
+
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,WindowManager.LayoutParams.TYPE_STATUS_BAR);
+
         String heading = getIntent().getStringExtra("heading");
         String description = getIntent().getStringExtra("description");
 
-        TextView headingText = findViewById(R.id.head);
-        TextView descriptionText = findViewById(R.id.descri);
+//        TextView headingText = findViewById(R.id.head);
+//        TextView descriptionText = findViewById(R.id.descri);
 
-        headingText.setText(heading);
-        descriptionText.setText(description);
+//        headingText.setText(heading);
+//        descriptionText.setText(description);
+        followButton.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.colorPrimaryDark)));
 
+
+//     TODO   Clicked on follow button
+        followButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                followPressed();
+                Toast.makeText(QuestionActivity.this, "Follow Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
+    }
+
+    public void onBackPressed(){
+        Toast.makeText(this, "BackPressed", Toast.LENGTH_SHORT).show();
+        finish();
+    }
+
+    public void followPressed(){
 
     }
 }
